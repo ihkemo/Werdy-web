04efcf49aeef15360b03ef6be8c6da148bb99d15
iHassan <kemo.exe@me.com>
2026-08-22 23:44:59 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
-const CACHE='werdy-web-v11';
+const CACHE='werdy-web-v17';
 const appAsset=path=>new URL(path,self.registration.scope).href;
-const CORE=['./','./index.html','./app.js','./manifest.json','./assets/quran/quran-data.js','./icons/icon-192.png','./icons/icon-512.png'].map(appAsset);
+const CORE=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/adhkar-data.js','./assets/quran/quran-data.js','./icons/icon-192.png','./icons/icon-512.png'].map(appAsset);
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
